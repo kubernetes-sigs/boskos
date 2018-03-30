sebastienvas/k8s-test-infra

Mason Implementation

boskos/boskos_test.go
@@ -26,15 +26,19 @@ import (
 	"time"
 
 	"k8s.io/test-infra/boskos/common"
+	"k8s.io/test-infra/boskos/crds"
 	"k8s.io/test-infra/boskos/ranch"
+	"k8s.io/test-infra/boskos/storage"
 )
 
 func MakeTestRanch(resources []common.Resource) *ranch.Ranch {
-	newRanch := &ranch.Ranch{
-		Resources: resources,
+	resourceClient := crds.NewTestResourceClient()
+	s, _ := ranch.NewStorage(storage.NewCRDStorage(resourceClient), "")
+	for _, r := range resources {
+		s.AddResource(r)
 	}
-
-	return newRanch
+	r, _ := ranch.NewRanch("", s)
+	return r
 }
 
 func TestAcquire(t *testing.T) {
@@ -181,8 +185,13 @@ func TestAcquire(t *testing.T) {
 				t.Errorf("%s - Got state %v, expect d", tc.name, data.State)
 			}
 
-			if c.Resources[0].Owner != "o" {
-				t.Errorf("%s - Wrong owner. Got %v, expect o", tc.name, c.Resources[0].Owner)
+			resources, err := c.Storage.GetResources()
+			if err != nil {
+				t.Error("cannot get resources")
+				continue
+			}
+			if resources[0].Owner != "o" {
+				t.Errorf("%s - Wrong owner. Got %v, expect o", tc.name, resources[0].Owner)
 			}
 		}
 	}
@@ -301,12 +310,17 @@ func TestRelease(t *testing.T) {
 		}
 
 		if rr.Code == http.StatusOK {
-			if c.Resources[0].State != "d" {
-				t.Errorf("%s - Wrong state. Got %v, expect d", tc.name, c.Resources[0].State)
+			resources, err := c.Storage.GetResources()
+			if err != nil {
+				t.Error("cannot get resources")
+				continue
+			}
+			if resources[0].State != "d" {
+				t.Errorf("%s - Wrong state. Got %v, expect d", tc.name, resources[0].State)
 			}
 
-			if c.Resources[0].Owner != "" {
-				t.Errorf("%s - Wrong owner. Got %v, expect empty", tc.name, c.Resources[0].Owner)
+			if resources[0].Owner != "" {
+				t.Errorf("%s - Wrong owner. Got %v, expect empty", tc.name, resources[0].Owner)
 			}
 		}
 	}
@@ -591,6 +605,21 @@ func TestUpdate(t *testing.T) {
 			code:   http.StatusOK,
 			method: http.MethodPost,
 		},
+		{
+			name: "ok",
+			resources: []common.Resource{
+				{
+					Name:       "res",
+					Type:       "t",
+					State:      "s",
+					Owner:      "merlin",
+					LastUpdate: FakeNow,
+				},
+			},
+			path:   "?name=res&state=s&owner=merlin",
+			code:   http.StatusOK,
+			method: http.MethodPost,
+		},
 	}
 
 	for _, tc := range testcases {
@@ -612,7 +641,12 @@ func TestUpdate(t *testing.T) {
 		}
 
 		if rr.Code == http.StatusOK {
-			if c.Resources[0].LastUpdate == FakeNow {
+			resources, err := c.Storage.GetResources()
+			if err != nil {
+				t.Error("cannot get resources")
+				continue
+			}
+			if resources[0].LastUpdate == FakeNow {
 				t.Errorf("%s - Timestamp is not updated!", tc.name)
 			}
 		}
@@ -743,27 +777,26 @@ func TestDefault(t *testing.T) {
 	}
 }
 
-func TestProjectConfig(t *testing.T) {
-	r := MakeTestRanch([]common.Resource{})
-	data, err := r.ParseConfig("resources.json")
+func TestConfig(t *testing.T) {
+	resources, err := ranch.ParseConfig("resources.yaml")
 	if err != nil {
 		t.Errorf("parseConfig error: %v", err)
 	}
 
-	if len(data) == 0 {
+	if len(resources) == 0 {
 		t.Errorf("empty data")
 	}
+	resourceNames := map[string]bool{}
 
-	names := map[string]bool{}
-	for _, p := range data {
+	for _, p := range resources {
 		if p.Name == "" {
 			t.Errorf("empty resource name: %v", p.Name)
 		}
 
-		if _, ok := names[p.Name]; ok {
+		if _, ok := resourceNames[p.Name]; ok {
 			t.Errorf("duplicated resource name: %v", p.Name)
 		} else {
-			names[p.Name] = true
+			resourceNames[p.Name] = true
 		}
 	}
 }

boskos/storage/storage_test.go
@@ -1,0 +1,133 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package storage
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+
+	"k8s.io/test-infra/boskos/common"
+	"k8s.io/test-infra/boskos/crds"
+	"reflect"
+)
+
+func createStorages() []PersistenceLayer {
+	return []PersistenceLayer{
+		NewCRDStorage(crds.NewTestResourceClient()),
+		NewMemoryStorage(),
+	}
+}
+
+func TestAddDelete(t *testing.T) {
+	for _, s := range createStorages() {
+		var resources []common.Resource
+		var err error
+		for i := 0; i < 10; i++ {
+			resources = append(resources, common.Resource{
+				Name: fmt.Sprintf("res-%d", i),
+				Type: fmt.Sprintf("type-%d", i),
+			})
+		}
+		sort.Stable(common.ResourceByName(resources))
+		for _, res := range resources {
+			if err = s.Add(res); err != nil {
+				t.Errorf("unable to add %s, %v", res.Name, err)
+			}
+		}
+		items, err := s.List()
+		if err != nil {
+			t.Errorf("unable to to list resources, %v", err)
+		}
+		var rResources []common.Resource
+		for _, i := range items {
+			var r common.Resource
+			r, err = common.ItemToResource(i)
+			if err != nil {
+				t.Errorf("unable to convert resource, %v", err)
+			}
+			rResources = append(rResources, r)
+		}
+		sort.Stable(common.ResourceByName(rResources))
+		if !reflect.DeepEqual(resources, rResources) {
+			t.Errorf("received resources (%v) do not match resources (%v)", resources, rResources)
+		}
+		for _, i := range items {
+			err = s.Delete(i.GetName())
+			if err != nil {
+				t.Errorf("unable to delete resource %s.%v", i.GetName(), err)
+			}
+		}
+		eResources, err := s.List()
+		if err != nil {
+			t.Errorf("unable to to list resources, %v", err)
+		}
+		if len(eResources) != 0 {
+			t.Error("list should return an empty list")
+		}
+	}
+}
+
+func TestUpdateGet(t *testing.T) {
+	for _, s := range createStorages() {
+		oRes := common.Resource{
+			Name: "original",
+			Type: "type",
+		}
+		if err := s.Add(oRes); err != nil {
+			t.Errorf("unable to add resource, %v", err)
+		}
+		uRes := oRes
+		uRes.Type = "typeUpdated"
+		if err := s.Update(uRes); err != nil {
+			t.Errorf("unable to update resource %v", err)
+		}
+		i, err := s.Get(oRes.Name)
+		if err != nil {
+			t.Errorf("unable to get resource, %v", err)
+		}
+		res, err := common.ItemToResource(i)
+		if err != nil {
+			t.Errorf("unable to convert resource, %v", err)
+		}
+		if !reflect.DeepEqual(uRes, res) {
+			t.Errorf("expected (%v) and received (%v) do not match", uRes, res)
+		}
+	}
+}
+
+func TestNegativeDeleteGet(t *testing.T) {
+	for _, s := range createStorages() {
+		oRes := common.Resource{
+			Name: "original",
+			Type: "type",
+		}
+		if err := s.Add(oRes); err != nil {
+			t.Errorf("unable to add resource, %v", err)
+		}
+		uRes := common.Resource{
+			Name: "notExist",
+			Type: "type",
+		}
+		if err := s.Update(uRes); err == nil {
+			t.Errorf("should not be able to update resource, %v", err)
+		}
+		if err := s.Delete(uRes.Name); err == nil {
+			t.Errorf("should not be able to delete resource, %v", err)
+		}
+	}
+}

kubernetes-sigs/boskos

boskos_test.go
@@ -26,15 +26,18 @@ import (
 	"time"
 
 	"k8s.io/test-infra/boskos/common"
+	"k8s.io/test-infra/boskos/crds"
 	"k8s.io/test-infra/boskos/ranch"
 )
 
 func MakeTestRanch(resources []common.Resource) *ranch.Ranch {
-	newRanch := &ranch.Ranch{
-		Resources: resources,
+	resourceClient := crds.NewTestResourceClient()
+	s, _ := ranch.NewStorage(crds.NewCRDStorage(resourceClient), "")
+	for _, r := range resources {
+		s.AddResource(r)
 	}
-
-	return newRanch
+	r, _ := ranch.NewRanch("", s)
+	return r
 }
 
 func TestAcquire(t *testing.T) {
@@ -181,8 +184,13 @@ func TestAcquire(t *testing.T) {
 				t.Errorf("%s - Got state %v, expect d", tc.name, data.State)
 			}
 
-			if c.Resources[0].Owner != "o" {
-				t.Errorf("%s - Wrong owner. Got %v, expect o", tc.name, c.Resources[0].Owner)
+			resources, err := c.Storage.GetResources()
+			if err != nil {
+				t.Error("cannot get resources")
+				continue
+			}
+			if resources[0].Owner != "o" {
+				t.Errorf("%s - Wrong owner. Got %v, expect o", tc.name, resources[0].Owner)
 			}
 		}
 	}
@@ -301,12 +309,17 @@ func TestRelease(t *testing.T) {
 		}
 
 		if rr.Code == http.StatusOK {
-			if c.Resources[0].State != "d" {
-				t.Errorf("%s - Wrong state. Got %v, expect d", tc.name, c.Resources[0].State)
+			resources, err := c.Storage.GetResources()
+			if err != nil {
+				t.Error("cannot get resources")
+				continue
+			}
+			if resources[0].State != "d" {
+				t.Errorf("%s - Wrong state. Got %v, expect d", tc.name, resources[0].State)
 			}
 
-			if c.Resources[0].Owner != "" {
-				t.Errorf("%s - Wrong owner. Got %v, expect empty", tc.name, c.Resources[0].Owner)
+			if resources[0].Owner != "" {
+				t.Errorf("%s - Wrong owner. Got %v, expect empty", tc.name, resources[0].Owner)
 			}
 		}
 	}
@@ -591,6 +604,21 @@ func TestUpdate(t *testing.T) {
 			code:   http.StatusOK,
 			method: http.MethodPost,
 		},
+		{
+			name: "ok",
+			resources: []common.Resource{
+				{
+					Name:       "res",
+					Type:       "t",
+					State:      "s",
+					Owner:      "merlin",
+					LastUpdate: FakeNow,
+				},
+			},
+			path:   "?name=res&state=s&owner=merlin",
+			code:   http.StatusOK,
+			method: http.MethodPost,
+		},
 	}
 
 	for _, tc := range testcases {
@@ -612,7 +640,12 @@ func TestUpdate(t *testing.T) {
 		}
 
 		if rr.Code == http.StatusOK {
-			if c.Resources[0].LastUpdate == FakeNow {
+			resources, err := c.Storage.GetResources()
+			if err != nil {
+				t.Error("cannot get resources")
+				continue
+			}
+			if resources[0].LastUpdate == FakeNow {
 				t.Errorf("%s - Timestamp is not updated!", tc.name)
 			}
 		}
@@ -743,27 +776,26 @@ func TestDefault(t *testing.T) {
 	}
 }
 
-func TestProjectConfig(t *testing.T) {
-	r := MakeTestRanch([]common.Resource{})
-	data, err := r.ParseConfig("resources.json")
+func TestConfig(t *testing.T) {
+	resources, err := ranch.ParseConfig("resources.yaml")
 	if err != nil {
 		t.Errorf("parseConfig error: %v", err)
 	}
 
-	if len(data) == 0 {
+	if len(resources) == 0 {
 		t.Errorf("empty data")
 	}
+	resourceNames := map[string]bool{}
 
-	names := map[string]bool{}
-	for _, p := range data {
+	for _, p := range resources {
 		if p.Name == "" {
 			t.Errorf("empty resource name: %v", p.Name)
 		}
 
-		if _, ok := names[p.Name]; ok {
+		if _, ok := resourceNames[p.Name]; ok {
 			t.Errorf("duplicated resource name: %v", p.Name)
 		} else {
-			names[p.Name] = true
+			resourceNames[p.Name] = true
 		}
 	}
 }

storage_test.go
@@ -1,0 +1,135 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+	"testing"
+
+	"k8s.io/test-infra/boskos/common"
+	"k8s.io/test-infra/boskos/crds"
+	"k8s.io/test-infra/boskos/storage"
+)
+
+func createStorages() []storage.PersistenceLayer {
+
+	return []storage.PersistenceLayer{
+		crds.NewCRDStorage(crds.NewTestResourceClient()),
+		storage.NewMemoryStorage(),
+	}
+}
+
+func TestAddDelete(t *testing.T) {
+	for _, s := range createStorages() {
+		var resources []common.Resource
+		var err error
+		for i := 0; i < 10; i++ {
+			resources = append(resources, common.Resource{
+				Name: fmt.Sprintf("res-%d", i),
+				Type: fmt.Sprintf("type-%d", i),
+			})
+		}
+		sort.Stable(common.ResourceByName(resources))
+		for _, res := range resources {
+			if err = s.Add(res); err != nil {
+				t.Errorf("unable to add %s, %v", res.Name, err)
+			}
+		}
+		items, err := s.List()
+		if err != nil {
+			t.Errorf("unable to to list resources, %v", err)
+		}
+		var rResources []common.Resource
+		for _, i := range items {
+			var r common.Resource
+			r, err = common.ItemToResource(i)
+			if err != nil {
+				t.Errorf("unable to convert resource, %v", err)
+			}
+			rResources = append(rResources, r)
+		}
+		sort.Stable(common.ResourceByName(rResources))
+		if !reflect.DeepEqual(resources, rResources) {
+			t.Errorf("received resources (%v) do not match resources (%v)", resources, rResources)
+		}
+		for _, i := range items {
+			err = s.Delete(i.GetName())
+			if err != nil {
+				t.Errorf("unable to delete resource %s.%v", i.GetName(), err)
+			}
+		}
+		eResources, err := s.List()
+		if err != nil {
+			t.Errorf("unable to to list resources, %v", err)
+		}
+		if len(eResources) != 0 {
+			t.Error("list should return an empty list")
+		}
+	}
+}
+
+func TestUpdateGet(t *testing.T) {
+	for _, s := range createStorages() {
+		oRes := common.Resource{
+			Name: "original",
+			Type: "type",
+		}
+		if err := s.Add(oRes); err != nil {
+			t.Errorf("unable to add resource, %v", err)
+		}
+		uRes := oRes
+		uRes.Type = "typeUpdated"
+		if err := s.Update(uRes); err != nil {
+			t.Errorf("unable to update resource %v", err)
+		}
+		i, err := s.Get(oRes.Name)
+		if err != nil {
+			t.Errorf("unable to get resource, %v", err)
+		}
+		res, err := common.ItemToResource(i)
+		if err != nil {
+			t.Errorf("unable to convert resource, %v", err)
+		}
+		if !reflect.DeepEqual(uRes, res) {
+			t.Errorf("expected (%v) and received (%v) do not match", uRes, res)
+		}
+	}
+}
+
+func TestNegativeDeleteGet(t *testing.T) {
+	for _, s := range createStorages() {
+		oRes := common.Resource{
+			Name: "original",
+			Type: "type",
+		}
+		if err := s.Add(oRes); err != nil {
+			t.Errorf("unable to add resource, %v", err)
+		}
+		uRes := common.Resource{
+			Name: "notExist",
+			Type: "type",
+		}
+		if err := s.Update(uRes); err == nil {
+			t.Errorf("should not be able to update resource, %v", err)
+		}
+		if err := s.Delete(uRes.Name); err == nil {
+			t.Errorf("should not be able to delete resource, %v", err)
+		}
+	}
+}