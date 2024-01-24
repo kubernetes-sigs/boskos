e97093a5f3ce6d559d830b5c89907aa7a0191775
David Cheung <cheungdavid@google.com>
2024-01-24 18:59:50 +0000

    ssl-policies should be cleaned up after target-https-proxies.

--- cmd/janitor/gcp_janitor.py
+++ cmd/janitor/gcp_janitor.py
@@ -40,8 +40,6 @@ RESOURCES_BY_API = {
 
     # compute resources
     'compute.googleapis.com': [
-        Resource('', 'compute', 'ssl-policies', None, 'global', None, False, True, None),
-        Resource('', 'compute', 'ssl-policies', None, 'region', None, False, True, None),
         Resource('', 'compute', 'instances', None, 'zone', None, False, True, None),
         Resource('', 'compute', 'addresses', None, 'global', None, False, True, None),
         Resource('', 'compute', 'addresses', None, 'region', None, False, True, None),
@@ -55,6 +53,8 @@ RESOURCES_BY_API = {
         Resource('', 'compute', 'target-https-proxies', None, 'global', None, False, True, None),
         Resource('', 'compute', 'target-https-proxies', None, 'region', None, False, True, None),
         Resource('', 'compute', 'target-tcp-proxies', None, None, None, False, True, None),
+        Resource('', 'compute', 'ssl-policies', None, 'global', None, False, True, None),
+        Resource('', 'compute', 'ssl-policies', None, 'region', None, False, True, None),
         Resource('', 'compute', 'ssl-certificates', None, 'global', None, False, True, None),
         Resource('', 'compute', 'ssl-certificates', None, 'region', None, False, True, None),
         Resource('', 'compute', 'url-maps', None, 'global', None, False, True, None),
